2bcadfa9cc680d167654cebf8384384e1a685905
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 22:51:26 +0530

    Update CounterList.js

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+/*import React,{useContext} from 'react';
 import { counterContext } from '../counterContext';
 import { Counter } from './Counter';
 
@@ -17,4 +17,29 @@ export const CounterList = () =>{
             <li><Counter key={3} id="3" value={count} onClick={onClick}/></li>
         </ol>
     )
-}
+}*/
+import React, { useContext } from 'react';
+import { counterContext } from '../counterContext';
+import { Counter } from './Counter';
+
+export const CounterList = () => {
+	const { count, setCount } = useContext(counterContext);
+
+	const onClick = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<ol>
+			<li>
+				<Counter key={1} id="1" value={count} onClick={onClick} />
+			</li>
+			<li>
+				<Counter key={2} id="2" value={count} onClick={onClick} />
+			</li>
+			<li>
+				<Counter key={3} id="3" value={count} onClick={onClick} />
+			</li>
+		</ol>
+	);
+};
